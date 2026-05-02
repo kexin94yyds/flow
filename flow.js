85759669434d0355a367f4af3fcdc229f0dfe396
kexin94yyds <your.email@example.com>
2026-05-02 20:54:17 +0800

    iterate-checkpoint:2026-05-02T12:54:17Z | 自动检查点 20:54:17
    checkpoint_id: cp_20260502125417_fb583558
    turn_id: 8e3fc561-98b9-4a40-89c8-57e65868e8f0

--- flow.js
+++ flow.js
@@ -767,6 +767,12 @@
     } else if (mode === 'book') {
       icon = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/>`;
       text = '点击右上角"添加内容"添加书籍';
+    } else if (mode === 'audio') {
+      icon = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"/>`;
+      text = '点击右上角"添加内容"添加音频';
+    } else if (mode === 'web') {
+      icon = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"/>`;
+      text = '点击右上角"添加内容"添加网页';
     } else {
       icon = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>`;
       text = '点击右上角"添加内容"添加论文';
