79f0c21534303bebdb1bb5be92520bb03696fb49
kexin94yyds <your.email@example.com>
2026-05-12 15:03:33 +0800

    iterate-checkpoint:2026-05-12T07:03:33Z | 自动检查点 15:03:33
    checkpoint_id: cp_20260512070333_7967ae5c
    turn_id: 59f22a75-7c69-4836-8e25-9bbd64c395d6

--- config.local.js
+++ config.local.js
@@ -1,3 +1,4 @@
 var LOCAL_LANDING_PAGE_PATTERNS = [];
 var LOCAL_CUSTOM_GROUPS = [];
+var LOCAL_CUSTOM_NEW_TAB_LABEL = 'Tobooks';
 var LOCAL_CUSTOM_NEW_TAB_URL = 'https://tobooks.xin/tobooks-main/';
